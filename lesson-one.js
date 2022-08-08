2c4b492f53a643d13eab79cb4290caf4df2147a6
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-07 21:35:16 -0400

    Implemented a for loop for the fizzbuzz solution

--- lesson-one.js
+++ lesson-one.js
@@ -131,7 +131,22 @@ function reverseString(str) {
   
   // CHALLENGE 6: FIZZBUZZ
   // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-  function fizzBuzz() {}
+  function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+
+        } else {
+            console.log(i);
+        }
+    }
+  }
   
   
   
@@ -140,8 +155,8 @@ function reverseString(str) {
 //   const output = isPalindrome('racecar');
 //   const output = reverseInt('125');
 //   const output = capitalizeLetters('I Love JavaScript');
-const output = maxCharacter('javascript');
-
+//   const output = maxCharacter('javascript');
+const output = fizzBuzz();
 
   
   console.log(output);
